e726793918a48fc0c88987f092329f7daa80263d
cgwhitehead <chris.g.whitehead.83@gmail.com>
2019-02-25 20:28:40 -0600

    updated front and docs

--- optimize.py
+++ optimize.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from tqdm import tqdm 
 from random import randint, random
 from matplotlib import pyplot
 
@@ -50,36 +51,57 @@ def walking(steps):
     iters = steps
     top_happiness = -99999
     counter = 1
-    graph_x=[]
-    graph_y=[]
-    graph_y2=[]
+    #graph_x=[]
+    #graph_y=[]
+    #graph_y2=[]
     temp = 30
     hunting_list = first_list_builder()
+    best_list = []
+    pbar=tqdm(total=steps)
     while counter <= iters:
+        np.seterr(all='ignore')
+        pbar.update(1)
         counter+=1
         score1=evaluate_list(hunting_list)
         new_hunting_list=change_list(hunting_list)
         score2=evaluate_list(new_hunting_list)
+        if top_happiness > 900:
+            #print(menu[menu['id'].isin(best_list)])
+            pbar.close()
+            print('Finished early!')
+            return {'menu':menu[menu['id'].isin(best_list)][['animal', 'happiness', 'cost']]
+                        ,'cost':sum(Cost[best_list-1])
+                        ,'happiness':evaluate_list(best_list)
+        }
+        if score1>top_happiness:
+            top_happiness=score1
+            best_list=hunting_list.copy()
+        if score2>top_happiness:
+            top_happiness=score2
+            best_list=new_hunting_list.copy()
         if score2 > score1:
             hunting_list=new_hunting_list.copy()
-            if score1>top_happiness:
-                top_happiness=score1
-                hunting_list=new_hunting_list.copy()
         else:
             p = np.exp(-(score1)/temp)
             if random()<p:
                 hunting_list=new_hunting_list
-        temp = 50*np.sin(2*np.pi*counter/(steps/5))
+        temp = 50*np.sin(2*np.pi*counter/(steps/5), dtype=np.longdouble)
         #temp=30
-        print(score1, score2)
-        graph_x.append(counter)
-        graph_y.append(evaluate_list(hunting_list))
-        graph_y2.append(sum(Cost[hunting_list-1]))
-    pyplot.plot(graph_x,graph_y)
+        #print(score1, score2)
+        #graph_x.append(counter)
+        #graph_y.append(evaluate_list(hunting_list))
+        #graph_y2.append(sum(Cost[hunting_list-1]))
+    #pyplot.plot(graph_x,graph_y)
+    ## Comment console output used for tuning steps
     #pyplot.plot(graph_x,graph_y2)
-    print(menu[menu['id'].isin(hunting_list)])
-    print('Cost: {}'.format(sum(Cost[hunting_list-1])))
-    print('Happy: {}'.format(evaluate_list(hunting_list)))
+    #print(menu[menu['id'].isin(best_list)])
+    #print('Cost: {}'.format(sum(Cost[best_list-1])))
+    #print('Happy: {}'.format(evaluate_list(best_list)))
+    pbar.close()
+    return {'menu':menu[menu['id'].isin(best_list)][['animal', 'happiness', 'cost']]
+                        ,'cost':sum(Cost[best_list-1])
+                        ,'happiness':evaluate_list(best_list)
+        }
 
     
     
--- readme.md
+++ readme.md
@@ -16,6 +16,31 @@ Given your energy budget, the optimizer will give you, the monster, a hunting li
 
 Considering all the available animals, you don't have the time or the monster math chops to think through each combo. You're also worried that an optimization algorithm might be lazy and give you a 'better' result, but not the bets (ie local maximum).
 
+## Requirements
+* (pandas)[https://pandas.pydata.org/]
+* (numpy)[http://www.numpy.org/]
+* (tqdm)[https://github.com/tqdm/tqdm]
+
+## Optional
+* (matplotlib)[https://matplotlib.org/] Several plotting functions are commented out and are useful for exploring the optimization process. 
+
+## Running
+```
+python monster.py
+```
+Then answer Y or N.
+
+## Thoughts
+I wanted to work on a problem with more than one dimension without previous bounds. A sports team for example has a given number of members with a set position configuration.
+For the monster problem I removed the bounds on how many animals could be on the list, meaning the optimizer can go for high cost, high ROI or several low cost
+low ROI. At a later date I'd like to analyze the configurations produced by the optimizer. This could be used to develop a shortcut strategy, create bounds, and speed up optimization
+
+Speed was another consideration, the temperature strategy here forces breaking local maxes and trying again over and over which creates
+several optimal menus. If time weren't an option this could run for a long period of time and the best menu could be selected.
+
+Instead, (after a lot of runs) I chose 900 happiness to be a good enough menu and so if a menu is found that meets 900 happiness we
+call it good enough and return the list so as not to waste too much of the 'Monsters' time. 
+
 This optimizer uses [simlated annealing](https://en.wikipedia.org/wiki/Simulated_annealing) and a novel approach at temperature control.
 ## Data
 Courtesy of [Mockaroo](https://www.mockaroo.com)
